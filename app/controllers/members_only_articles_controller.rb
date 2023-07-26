6c5ff625f020e0150c6ae7ff78c543e688a2956c
GeralmyUrena <geralmyuri11@gmail.com>
2023-07-26 10:07:15 -0400

    commit tree c03b549a00871d92c1d02cd54d47006b1273e7cc

--- app/controllers/members_only_articles_controller.rb
+++ app/controllers/members_only_articles_controller.rb
@@ -1,5 +1,6 @@
 class MembersOnlyArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  before_action :authorize
 
   def index
     articles = Article.where(is_member_only: true).includes(:user).order(created_at: :desc)
@@ -12,9 +13,18 @@ class MembersOnlyArticlesController < ApplicationController
   end
 
   private
-
+  
+  def authorize 
+    return render json:{error: "Not authorized"},status: :unauthorized unless session.include? :user_id
+  
+  end
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
   end
 
 end
+
+# - If a user is not signed in, the `#index` and `#show` actions should return a
+#   status code of 401 unauthorized, along with an error message
+# - If the user is signed in, the `#index` and `#show` actions should return the
+#   JSON data for the members-only articles.
